78b51ef87b65e85f615dd146580231aa17e9a565
dikoleshubh <dikole.shubh@gmail.com>
2021-06-03 10:39:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll_ADO_DOT_NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
