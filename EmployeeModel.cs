27995d20fdc8cc1eaa9fd8efa4b5562da8a6194a
dikoleshubh <dikole.shubh@gmail.com>
2021-06-04 08:56:01 +0530

    Modified Salay

---
+++ EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll_ADO_DOT_NET
+{
+    public class EmployeeModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double basic_pay { get; set; }
+        public DateTime start_date { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }
+        public string phone_number { get; set; }
+        public string department { get; set; }
+        public double deduction { get; set; }
+        public double taxable_pay { get; set; }
+        public double income_tax { get; set; }
+        public double net_pay { get; set; }
+    }
+}
--- Employeerepo.cs
+++ Employeerepo.cs
@@ -31,6 +31,77 @@ namespace Payroll_ADO_DOT_NET
                 this.connection.Close();
             }
 
+        public void AddEmployee(e model)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    //Creating a stored Procedure for adding employees into database
+                    SqlCommand command = new SqlCommand("dbo.Employee_Daata", this.connection);
+                    //Command type is set as stored procedure
+                    command.CommandType = CommandType.StoredProcedure;
+                    //Adding values from employeemodel to stored procedure using disconnected architecture
+                    //connected architecture will only read the data
+                    command.Parameters.AddWithValue("@EmpName", model.name);
+                    command.Parameters.AddWithValue("@basic_Pay", model.basic_pay);
+                    command.Parameters.AddWithValue("@StartDate", model.start_date);
+                    command.Parameters.AddWithValue("@gender", model.gender);
+                    command.Parameters.AddWithValue("@phoneNumber", model.phone_number);
+                    command.Parameters.AddWithValue("@department", model.department);
+                    command.Parameters.AddWithValue("@address", model.address);
+                    command.Parameters.AddWithValue("@deductions", model.deduction);
+                    command.Parameters.AddWithValue("@taxable_pay", model.taxable_pay);
+                    command.Parameters.AddWithValue("@income_tax", model.income_tax);
+                    command.Parameters.AddWithValue("@net_pay", model.net_pay);
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    connection.Close();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+        public bool UpdateSalaryIntoDatabase(string empName, double basicPay)
+        {
+            SqlConnection connection= new SqlConnection(connectionString);
+           
+            try
+            {
+                using (connection)
+                {
+                    connection.Open();
+                    string query = @"update dbo.employee_payroll set basic_pay=@p1 where EmpName=@p2";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@p1", basicPay);
+                    command.Parameters.AddWithValue("@p2", empName);
+                    var result = command.ExecuteNonQuery();
+                    connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
